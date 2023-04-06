994b06fce10cc479ac568d2a6d66cb39edbbb127
Felix Luciano Salomon <felixluciano.a@gmail.com>
2023-04-05 19:54:15 -0700

    Fixed github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="p-10">
       <a
-        href="https://www.linkedin.com/in/felix-luciano-salomon-6266021b6/"
+        href="https://www.github.com/faluciano"
         className=" mb-1 mr-1 inline-flex items-center rounded-full border border-white bg-white p-2 text-center"
       >
         <FaGithub size={"2em"} className="self-center text-center text-black" />
