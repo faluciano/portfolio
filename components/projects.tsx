3be122bf2c83dd57acb19839e369eeea95344cd7
Felix Luciano Salomon <felixluciano.a@gmail.com>
2023-04-05 23:37:07 -0700

    Added project cards

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,8 +1,36 @@
+import ProjectCard from "./projectcard";
+
 const Projects = () => {
+  const ProjectData = [
+    {
+      title: "Instagram",
+      description:
+        "Instagram clone for android written in Java with Parse as a backend",
+      url: "https://github.com/faluciano/Instagram",
+    },
+    {
+      title: "Flixster",
+      description: "Movie browsing app for android that uses TMDB",
+      url: "https://github.com/faluciano/Flixster",
+    },
+  ];
   return (
-    <div id="projects" className="text-center text-5xl">
-      Projects
-    </div>
+    <>
+      <div id="projects" className="text-center text-5xl">
+        Projects
+      </div>
+      {ProjectData.map((project) => {
+        return (
+          <div key={project.title} className="mb-4 ml-4 mr-4 mt-4 inline-flex">
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              url={project.url}
+            />
+          </div>
+        );
+      })}
+    </>
   );
 };
 
